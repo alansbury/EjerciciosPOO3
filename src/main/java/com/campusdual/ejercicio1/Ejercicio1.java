0c0ce08a6d7f271c97e7fbacba3af36b62b09c18
Angel Castineira <angel.r.castineira@gmail.com>
2023-09-05 13:16:11 +0200

    Ejercicio2

--- src/main/java/com/campusdual/ejercicio1/Ejercicio1.java
+++ src/main/java/com/campusdual/ejercicio1/Ejercicio1.java
@@ -6,7 +6,27 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
+
+       int year = 1960;
+       boolean bisiesto = false;
+       if (year%4 == 0){
+           if (year%100 == 0){
+               if (year%400 == 0){
+                   bisiesto = true;
+               }else{
+                   bisiesto = false;
+               }
+           }else{
+               bisiesto = true;
+           }
+       }else {
+           bisiesto = false;
+       }
+
+
+
+
+
+
     }
-
-
 }
---
+++ src/main/java/com/campusdual/ejercicio2/Ejercicio2.java
@@ -0,0 +1,38 @@
+package com.campusdual.ejercicio2;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        while (true){
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Introduce un ano");
+        Integer year = scanner1.nextInt();
+
+        boolean bisiesto = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    bisiesto = true;
+                } else {
+                    bisiesto = false;
+                }
+            } else {
+                bisiesto = true;
+            }
+        } else {
+            bisiesto = false;
+        }
+
+        if (bisiesto == true) {
+            System.out.println("bisiesto");
+        } else {
+            System.out.println("non bisiesto");
+
+
+        }
+
+    }
+    }
+}
