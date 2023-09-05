fdf3df55340fca5102e78eecdf0281f14f1cdea1
Angel Castineira <angel.r.castineira@gmail.com>
2023-09-05 11:00:49 +0200

    ejercici1 hello world

---
+++ src/main/java/com/campusdual/ejercicio1/Ejercicio1.java
@@ -0,0 +1,12 @@
+package com.campusdual.ejercicio1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+
+}
